7976e084d87c238d36e3557119623afe033654da
venika-sem <99354917+venika-sem@users.noreply.github.com>
2022-03-21 23:53:37 +0000

    changed main.cs

--- main.cs
+++ main.cs
@@ -1,7 +1,15 @@
+// Created by: Venika Sem
+// Created on: Mar 2022
+//
+// This program displays, "Hello, World!"
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello, World!");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("\nDone.");
+    }
 }
